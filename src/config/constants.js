84bb8bf57e8cb1edad6f3d514c3da2e457a9fc75
GiovannaR <giovannariqueti@gmail.com>
2019-12-03 00:13:23 -0300

    Add constant

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  // URL_HOST: "https://teste-personalidade-backend.herokuapp.com/api",
+  URL_HOST: "https://fronteventos.herokuapp.com/api",
   URL_HOST: "http://localhost:5000/api",
   ADMIN_PASSWORD: "1234"
 };
